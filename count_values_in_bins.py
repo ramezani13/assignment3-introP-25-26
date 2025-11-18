06d2d7fa41daa5ba10323d7b48d2d865fe62bd7c
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-11-18 23:57:43 +0100

    count_values_in_bins.py

--- count_values_in_bins.py
+++ count_values_in_bins.py
@@ -1,7 +1,7 @@
-def is_odd(k):
-    """
-    Return True if k is odd, otherwise return False.
-    """
+import numpy as np
+
+def count_values_in_bins(data, bin_edges):
     ### Replace with your own code (begin) ###
     pass
     ### Replace with your own code (end)   ###
+
